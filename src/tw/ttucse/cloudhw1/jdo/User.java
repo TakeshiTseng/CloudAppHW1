5df90e1775b26d57ecc798a969c4beab61403436
Taeshi <a86487817@gmail.com>
2013-10-28 10:45:34 +0800

    fix login bug

--- src/tw/ttucse/cloudhw1/jdo/User.java
+++ src/tw/ttucse/cloudhw1/jdo/User.java
@@ -1,9 +1,13 @@
 package tw.ttucse.cloudhw1.jdo;
 
 import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
 public class User {
 	@PrimaryKey
     @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
